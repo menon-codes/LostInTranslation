9db4e1005c285eff044d49aaf129d4d5e17b4dad
Aditya Menon <adimenon@outlook.com>
2025-09-24 15:39:23 -0400

    feat: completed task D

--- src/main/java/translation/GUI.java
+++ src/main/java/translation/GUI.java
@@ -3,20 +3,17 @@ package translation;
 import javax.swing.*;
 import java.awt.event.*;
 
-
-// TODO Task D: Update the GUI for the program to align with UI shown in the README example.
-//            Currently, the program only uses the CanadaTranslator and the user has
-//            to manually enter the language code they want to use for the translation.
-//            See the examples package for some code snippets that may be useful when updating
-//            the GUI.
 public class GUI {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+            Translator translator = new JSONTranslator();
+
             JPanel countryPanel = new JPanel();
             JTextField countryField = new JTextField(10);
+            // is it still suppsoed to be only "can" allowed?
             countryField.setText("can");
-            countryField.setEditable(false); // we only support the "can" country code for now
+            countryField.setEditable(false);
             countryPanel.add(new JLabel("Country:"));
             countryPanel.add(countryField);
 
@@ -34,26 +31,18 @@ public class GUI {
             JLabel resultLabel = new JLabel("\t\t\t\t\t\t\t");
             buttonPanel.add(resultLabel);
 
-
-            // adding listener for when the user clicks the submit button
             submit.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     String language = languageField.getText();
                     String country = countryField.getText();
 
-                    // for now, just using our simple translator, but
-                    // we'll need to use the real JSON version later.
-                    Translator translator = new CanadaTranslator();
-
                     String result = translator.translate(country, language);
                     if (result == null) {
                         result = "no translation found!";
                     }
                     resultLabel.setText(result);
-
                 }
-
             });
 
             JPanel mainPanel = new JPanel();
@@ -67,7 +56,6 @@ public class GUI {
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.pack();
             frame.setVisible(true);
-
 
         });
     }

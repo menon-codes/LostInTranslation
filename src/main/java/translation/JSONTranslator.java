9016f11354b11eb9903ebf1db113ca8f6faff577
Aditya Menon <adimenon@outlook.com>
2025-09-24 15:14:45 -0400

    style: removed todos

--- src/main/java/translation/JSONTranslator.java
+++ src/main/java/translation/JSONTranslator.java
@@ -14,7 +14,8 @@ import org.json.JSONObject;
 
 /**
  * An implementation of the Translator interface that reads in the translation
- * data from a JSON file. The data is read in once each time an instance of this class is constructed.
+ * data from a JSON file. The data is read in once each time an instance of this
+ * class is constructed.
  */
 public class JSONTranslator implements Translator {
 
@@ -22,8 +23,10 @@ public class JSONTranslator implements Translator {
 
     private final List<String> countryCodes = new ArrayList<>();
 
-    // the key used is "countryCode-languageCode"; the value is the translated country name
+    // the key used is "countryCode-languageCode"; the value is the translated
+    // country name
     private final Map<String, String> translations = new HashMap<>();
+
     /**
      * Construct a JSONTranslator using data from the sample.json resources file.
      */
@@ -32,7 +35,9 @@ public class JSONTranslator implements Translator {
     }
 
     /**
-     * Construct a JSONTranslator populated using data from the specified resources file.
+     * Construct a JSONTranslator populated using data from the specified resources
+     * file.
+     * 
      * @param filename the name of the file in resources to load the data from
      * @throws RuntimeException if the resource file can't be loaded properly
      */
@@ -51,7 +56,6 @@ public class JSONTranslator implements Translator {
 
                 List<String> languages = new ArrayList<>();
 
-                // TODO Task C: record this countryCode in the correct instance variable
                 if (!countryCodes.contains(countryCode)) {
                     countryCodes.add(countryCode);
                 }
@@ -60,11 +64,10 @@ public class JSONTranslator implements Translator {
                 for (String key : countryData.keySet()) {
                     if (!key.equals("id") && !key.equals("alpha2") && !key.equals("alpha3")) {
                         String languageCode = key;
-                        // TODO Task C: record this translation in the appropriate instance variable
+
                         String translation = countryData.getString(languageCode);
                         String translateKey = countryCode + "_" + languageCode;
                         translations.put(translateKey, translation);
-
 
                         if (!languageCodes.contains(languageCode)) {
                             languageCodes.add(languageCode);
@@ -72,15 +75,14 @@ public class JSONTranslator implements Translator {
                     }
                 }
             }
-        }
-        catch (IOException | URISyntaxException ex) {
+        } catch (IOException | URISyntaxException ex) {
             throw new RuntimeException(ex);
         }
     }
 
     @Override
     public List<String> getLanguageCodes() {
-        // TODO Task C: return a copy of the language codes
+
         return new ArrayList<>(languageCodes);
     }
 
@@ -91,7 +93,7 @@ public class JSONTranslator implements Translator {
 
     @Override
     public String translate(String countryCode, String languageCode) {
-        // TODO Task C: complete this method using your instance variables as needed
+
         return translations.get(countryCode + "_" + languageCode);
 
     }
